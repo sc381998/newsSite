13797b80088d8f3068eca1741afbfc4b2669d826
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-09-21 12:27:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,98 @@
+//slider in should know componenet
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+      // nav:true
+    },
+    400: {
+      items: 2
+      // nav:true
+    },
+    600: {
+      items: 3
+      // nav:false
+    },
+    800: {
+      items: 4
+      // nav:false
+    },
+    1000: {
+      items: 5
+      // nav:true,
+      // loop:false
+    }
+  }
+});
+
+//IMAGE upload in form
+$(".custom-file-input").on("change", function () {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+
+$("#weather").click(function () {
+  document.getElementById("weather").classList.add("hide");
+  document.getElementById("siliguri-weather").classList.remove("hide");
+});
+
+//inlcude HTML
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+includeHTML();
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    document.getElementById("color-bar").classList.remove("hide");
+  } else {
+    navbar.classList.remove("sticky");
+    document.getElementById("color-bar").classList.add("hide");
+  }
+}
